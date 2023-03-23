031d68930d95f29e8dc95f6374980afa71d49b6d
Celia Taroncher <celiatr.17@gmail.com>
2023-03-23 01:32:48 +0100

    texto fuera

--- src/index.js
+++ src/index.js
@@ -22,28 +22,26 @@ getMovies.forEach((doc) => {
   </div>`;
 
   carta.appendChild(padre);
-
   // Accedemos al div modal que será el que nos muestre la info más detallada de la peli.
+  const btn = document.querySelector(".info");
   const modal = document.querySelector(".modal");
   const moreInfo = document.createElement("div");
 
-  moreInfo.innerHTML = `<div class="more-info"><h2 class="tittle-modal">${doc.id}</h2>
-  <p class="description">${doc.data().plot}</p> <p class="actors">${
-    doc.data().actors
-  }</p> <p class="director">${doc.data().director}</p>
-  <p class="genres">${doc.data().genres}</p> <p class="year">${doc.data().year}</p>
-  </div>`;
-
   modal.appendChild(moreInfo);
-  const btn = document.querySelector(".info");
   const close = document.querySelector(".close");
 
   btn.addEventListener("click", function () {
-    modal.style.display = "block";
+    window.alert("");
+    // modal.style.display = "block";
+    moreInfo.innerHTML = `<div class="more-info"><h2 class="tittle-modal">${doc.id}</h2>
+    <p class="description">${doc.data().plot}</p> <p class="actors">${
+      doc.data().actors
+    }</p> <p class="director">${doc.data().director}</p>
+    <p class="genres">${doc.data().genres}</p> <p class="year">${doc.data().year}</p>
+    </div>`;
   });
 
   close.addEventListener("click", function () {
     modal.style.display = "none";
   });
-
 });
